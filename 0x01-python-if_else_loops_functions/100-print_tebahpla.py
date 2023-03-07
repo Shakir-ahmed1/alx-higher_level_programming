19d7ec809ba3d5d2776f1c15376159f1cc214dfe
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-07 21:08:37 +0000

    smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for n in [""]:
-    pass
-print("zYxWvUtSrQpOnMlKjIhGfEdCbA".format(), end="")
+for n in range(13):
+    print("{}{}".format(chr(122-2 * n), chr(90-2 * n-1)), end="")
