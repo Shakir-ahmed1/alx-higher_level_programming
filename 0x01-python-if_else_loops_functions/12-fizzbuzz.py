bbf708acec504d9d65b121ed8673ed2327fbb74b
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-07 18:58:42 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in range(1,101):
+    for n in range(1, 101):
         if n % 15 == 0:
-            print("FizzBuzz",end=" ")
-            continue;
+            print("FizzBuzz", end=" ")
+            continue
         if n % 3 == 0:
             print("Fizz", end=" ")
         if n % 5 == 0:
