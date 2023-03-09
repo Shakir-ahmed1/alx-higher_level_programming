a49bcf77bed6a8ccaf822fa8b4ed86f133f5c6e7
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-09 18:58:55 +0000

    who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-for d in dir(hidden_4):
-    if d[:2] != "__":
-        print(d)
+if __name__ == '__main__':
+    for d in dir(hidden_4):
+        if d[:2] != "__":
+            print(d)
