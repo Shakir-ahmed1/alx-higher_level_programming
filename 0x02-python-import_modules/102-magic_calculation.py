7f2cb90b96a2455bceb2cccc1d572a4706dc6e31
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-09 18:37:06 +0000

    decompile

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,10 +3,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
+    for i in range(4, 6):
+        c = add(c, i)
+    return c
 
     return sub(a, b)
-from dis import dis
-dis(magic_calculation)
