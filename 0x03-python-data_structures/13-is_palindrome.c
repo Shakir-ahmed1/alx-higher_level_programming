09f20a38e5fd08881d924f69b4633221692193cf
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 15:36:40 +0000

    submitting final code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 #include <stdlib.h>
+int palind_checker(listint_t**, listint_t*);
 /**
  * is_palindrome - checks if the linked list value is symmetry
  * @head: the starting point of the linked list
  * Return: 0 if not palindrome. 1 if palindrome
  */
-int palind_checker(listint_t**, listint_t*);
 int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
-	return palind_checker(head, *head);
+	return (palind_checker(head, *head));
 }
+/**
+ * palind_checker - it checks if the given linked list is palindrome recursive
+ * @left: used to start from the first element in the linked list
+ * @right: used to push the index of the linked list to the end
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
 int palind_checker(listint_t **left, listint_t *right)
 {
 	int a;
@@ -20,13 +26,15 @@ int palind_checker(listint_t **left, listint_t *right)
 		return (1);
 	a = palind_checker(left, right->next);
 	if (a == 0)
-		return 0;
+		return (0);
 	if ((*left)->n == right->n)
 	{
 		(*left) = (*left)->next;
-		return 1;
+		return (1);
 	}
 	else
-		return 0;
+	{
+		return (0);
+	}
 
 }
