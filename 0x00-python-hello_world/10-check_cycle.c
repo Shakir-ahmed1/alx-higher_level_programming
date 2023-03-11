187441787cb59d9f3deea0c64a933587ce919aeb
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 14:39:22 +0000

    removing unused malloc

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,39 +1,21 @@
 #include "lists.h"
 /**
- * free_both - it frees two memories at once
- * @a: first mem
- * @b: second mem
- */
-void free_both(listint_t *a, listint_t *b)
-{
-	free(a);
-	free(b);
-}
-/**
  * check_cycle - it checks if a given linked list has a cycle
  * @list: the first list
  * Return: 0 if there is no cycle. 1 if ther is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp, *move, *t, *m;
+	listint_t *temp, *move;
 	int i = 0, j = 0;
 
-	temp = malloc(sizeof(listint_t));
-	move = malloc(sizeof(listint_t));
-	if (temp == NULL || temp == NULL)
-		return (1);
-	t = temp;
-	m = move;
 	temp = list;
 	move = list;
 	if (list == NULL || list->next == NULL)
 	{
-		free_both(t, m);
 		return (0); }
 	if (move == move->next)
 	{
-		free_both(t, m);
 		return (1); }
 	while (1)
 	{
@@ -44,15 +26,12 @@ int check_cycle(listint_t *list)
 		{
 			if (temp == move)
 			{
-				free_both(t, m);
 				return (1);
 			}
 			if (temp == NULL || move == NULL)
 			{
-				free_both(t, m);
 				return (0); }
 			move = move->next;
 			j++; }
 		i++; }
-	free_both(t, m);
 	return (0); }
