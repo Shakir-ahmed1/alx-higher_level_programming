7ad695fb3c7d58aa2d6ab4ce98cea2be164a8e23
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 14:25:29 +0000

    is palindrome1

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,25 +5,26 @@
  * @head: the starting point of the linked list
  * Return: 0 if not palindrome. 1 if palindrome
  */
-int isPalindromeUtil(listint_t **left, listint_t *right)
-{
-    if (right == NULL)
-        return 1;
-    int isp = isPalindromeUtil(left, right->next);
-    if (isp == 0)
-        return 0;
-
-    int isp1 = (right->n == (*left)->n);
-
-    *left = (*left)->next;
-
-    return isp1;
-}
-
+int isPalindromeUtil(listint_t **left, listint_t *right);
 int is_palindrome(listint_t **head)
 {
    return isPalindromeUtil(head, *head);
 }
+int isPalindromeUtil(listint_t **left, listint_t *right)
+{
+	int isp, isp1;
+
+	if (right == NULL)
+		return 1;
+	isp = isPalindromeUtil(left, right->next);
+	if (isp == 0)
+		return 0;
+	isp1 = (right->n == (*left)->n);
+	*left = (*left)->next;
+
+	return isp1;
+}
+
 /**
 int is_palindrome(listint_t **head)
 {
