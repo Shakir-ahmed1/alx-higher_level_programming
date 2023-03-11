7c709f1638208af968e3f6ac74961a27e5477886
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 09:13:06 +0000

    fixing for pycodestyle

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     max = my_list[0]
-    for l in my_list:
-        if l > max:
-            max = l
+    for ls in my_list:
+        if ls > max:
+            max = ls
     return max
