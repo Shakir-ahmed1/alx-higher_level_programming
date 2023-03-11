75666f2f6d25377791eab356ff187fa11eb91da9
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 09:32:49 +0000

    fixing correct out put fail

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        print('', end='')
     for m in range(1, len(my_list) + 1):
         print("{:d}".format(my_list[m * -1]))
