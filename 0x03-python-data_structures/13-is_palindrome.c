90ec52c2969b8546dff5950a5dcadf93a1109ccf
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-11 15:27:38 +0000

    code upload

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,27 +1,32 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * is_palindrome - checks if the linked list value is symmetry
  * @head: the starting point of the linked list
  * Return: 0 if not palindrome. 1 if palindrome
  */
-int isPalindromeUtil(listint_t **left, listint_t *right);
+int palind_checker(listint_t**, listint_t*);
 int is_palindrome(listint_t **head)
 {
-   return isPalindromeUtil(head, *head);
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
+	return palind_checker(head, *head);
 }
-int isPalindromeUtil(listint_t **left, listint_t *right)
+int palind_checker(listint_t **left, listint_t *right)
 {
-	int isp, isp1;
+	int a;
 
 	if (right == NULL)
-		return 1;
-	isp = isPalindromeUtil(left, right->next);
-	if (isp == 0)
+		return (1);
+	a = palind_checker(left, right->next);
+	if (a == 0)
 		return 0;
-	isp1 = (right->n == (*left)->n);
-	*left = (*left)->next;
+	if ((*left)->n == right->n)
+	{
+		(*left) = (*left)->next;
+		return 1;
+	}
+	else
+		return 0;
 
-	return isp1;
 }
-
-
