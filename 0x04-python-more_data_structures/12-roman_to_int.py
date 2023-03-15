a877effdb82e0f9ca8706fff99955a7608508d5d
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-15 14:04:46 +0000

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != type(' '):
+        return 0
     roman_num = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
     temp = ''
     result = 0
