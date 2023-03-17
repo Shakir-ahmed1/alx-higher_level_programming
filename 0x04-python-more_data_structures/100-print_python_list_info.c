ef0e3f4fdc2320413dc72f07f4b645bae76d75d9
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-17 11:53:44 +0000

    move files

---
+++ 0x04-python-more_data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "Python.h"
+/**
+ * print_python_list_info - it prints the info of the given list
+ * @p: the list
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *list;
+	int i;
+
+	list = (PyListObject *) p;
+	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+	for (i = 0; i < list->ob_base.ob_size; i++)
+	{
+		/* changing between -> and . may cause unpredictable error */
+		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	}
+}
