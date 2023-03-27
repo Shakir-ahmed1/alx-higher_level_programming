3050f58f7d4864a2b2ecd97c691fc48e48843dfb
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-27 11:28:09 +0000

    print safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        counter = 0
+        for ls in my_list:
+            if counter != x:
+                counter += 1
+                print(ls,end="")
+            else:
+                print()
+                return counter
+        print()
+        return counter
+    except:
+        raise
+        # print("error occuted while printing")
