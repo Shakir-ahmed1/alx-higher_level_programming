f26241a3bdf15642b7bd2cc6fb81e56af03211b8
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-28 21:12:12 +0000

    conditions for error handlers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -62,17 +62,14 @@ class Square:
     @position.setter
     def position(self, value):
         """Position setter"""
-        try:
-            if (not isinstance(value, tuple)
-                or len(value) != 2
-                or not isinstance(value[0], int)
-                or not isinstance(value[1], int)
-                or value[0] < 0 or value[1] < 0):
-                raise TypeError
-            self.__position = value
-        except TypeError:
-            print("position must be a tuple of 2 positive integers")
-            
+        if (not isinstance(value, tuple)
+            or len(value) != 2
+            or not isinstance(value[0], int)
+            or not isinstance(value[1], int)
+            or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+        
 
     def my_print(self):
         """Prints the size of square using #"""
