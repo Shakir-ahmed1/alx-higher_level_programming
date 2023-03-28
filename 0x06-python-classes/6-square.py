0bf2fc14288fc747f011931e75c6d954c1a5085b
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-28 21:14:00 +0000

    conditions for error handlers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,7 +68,8 @@ class Square:
             or not isinstance(value[1], int)
             or value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
         
 
     def my_print(self):
