35853f692d55382e478244f1890fd255a2400471
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-28 19:42:48 +0000

    bug fix error output

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,15 +7,15 @@ class Square:
     def __init__(self, size=0):
         """Initializes the data and handles error"""
         try:
-            if isinstance(size, str):
+            if not isinstance(size, int):
                 raise TypeError
             if size < 0:
                 raise ValueError
 
             self.__size = size
         except TypeError:
-            print("size must be an integer")
+            print("size must be an integer", end="")
             raise
         except ValueError:
-            print("size must be >= 0")
+            print("size must be >= 0", end="")
             raise
