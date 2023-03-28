28e111faca92012b88047377d3817376777bb0c4
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-28 19:09:28 +0000

    access and update private attribute - print a square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Square Modlue"""
+
+
+class Square:
+    """Square with a size"""
+    def __init__(self, size=0):
+        """Initializes the size and handles error"""
+        try:
+            if not isinstance(size, int):
+                raise TypeError
+            if size < 0:
+                raise ValueError
+
+            self.__size = size
+        except TypeError:
+            print("size must be an integer", end="")
+            raise
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise
+
+    def area(self):
+        """Calculates the area of the square"""
+        return (self.__size * self.__size)
+    @property
+    def size(self):
+        """get size"""
+        return (self.__size)
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        try:
+            if not isinstance(size, int):
+                raise TypeError
+            if size < 0:
+                raise ValueError
+
+            self.__size = size
+        except TypeError:
+            print("size must be an integer", end="")
+            raise
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise
