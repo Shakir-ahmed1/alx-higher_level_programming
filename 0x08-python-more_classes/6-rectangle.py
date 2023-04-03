601459f684f82c69a17803a57f1e2c840ea24686
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-03 22:37:19 +0000

    pycodestyle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@
 class Rectangle:
     """A class that represents rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initializes the classes with the given value
             Args:
