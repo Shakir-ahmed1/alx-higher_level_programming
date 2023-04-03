b5c8509311a33e2a4e7a306b5a7f4c8df94161cc
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-03 22:17:38 +0000

    Eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""
+    Defines a rectangle with width and height
+"""
+
+
+class Rectangle:
+    """A class that represents rectangle"""
+    def __init__(self, width=0, height=0):
+        """ Initializes the classes with the given value
+            Args:
+                width: the width of the rectangle
+                height: the height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Returns the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """The area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """The perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """prints the rectangle using # """
+        result = ""
+        if self.__width == 0 or self.__height == 0:
+            return result
+        for a in range(self.__height):
+            result += "#" * self.__width
+            if a != self.__height - 1:
+                result += "\n"
+        return result
+
+    def __repr__(self):
+        """returns the string that enables to recreate same rectangle"""
+        return f"Rectangle({self.__width},{self.__height})"
