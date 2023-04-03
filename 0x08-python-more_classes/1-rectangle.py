0aa1e3c055be8eb1e7f4c23c3e05281e206530ad
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-03 22:01:37 +0000

    bug fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,10 +13,14 @@ class Rectangle:
                 width: the width of the rectangle
                 height: the height of the rectangle
         """
-        if not isinstance(height, int) or not isinstance(width, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0 or height < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
