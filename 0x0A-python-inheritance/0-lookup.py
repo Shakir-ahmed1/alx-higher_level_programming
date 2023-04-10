8a92d68bbb5858080d35fb9efe481a5b851ea72f
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-10 15:05:50 +0000

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a module that prints list of attributes of an object"""
 
+
 def lookup(obj):
     """Returns a list of attributes of the given object"""
     return dir(obj)
