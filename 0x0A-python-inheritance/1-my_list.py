adc2cfb7c52dbe9dbaac9dd83d0a5fa9c0dbef16
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-10 20:00:40 +0000

    bug fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,11 +4,6 @@
 
 class MyList(list):
     """Enables sorted printing for the list object"""
-    def __init__(self, value=[]):
-        """Initializes the class"""
-        super().__init__()
-        self = value
-
     def print_sorted(self):
         """Sortes the given length by size"""
         print(sorted(self))
