4ff53c10f3461fbbfdee5d0b3743df2b893aac52
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-10 20:47:34 +0000

    geometry module

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a module with BaseGeometry calss"""
+
+
+class BaseGeometry:
+    """ A class with unimplimentd area"""
+    def area(self):
+        raise Exception("area() is not implemented")
