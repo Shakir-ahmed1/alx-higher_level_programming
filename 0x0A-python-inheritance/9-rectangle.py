90d4483dd1c4a4395cee126efda67299735f38c1
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-10 22:37:02 +0000

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" a module with BaseGeometry calss"""
+
+
+class BaseGeometry:
+    """ A class with unimplimentd area"""
+    def area(self):
+        """ An area with no implimentstion"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates integers for latter use"""
+        if not isinstance(value, int) or type(value) == bool:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle a geometry with 4 sides """
+    def __init__(self, width, height):
+        """ Initializes width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Area of the rectangle """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """ The string representation of the rectangle class """
+        return f"[Rectangel] {self.__width}/{self.__height}"
