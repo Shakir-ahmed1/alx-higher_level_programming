2d13eb61b99d9d518a0f18740f14decb32685718
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-10 22:21:40 +0000

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a module with BaseGeometry calss"""
+
+
+class BaseGeometry:
+    """ A class with unimplimentd area"""
+    def area(self):
+        """ An area with no implimentstion"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates integers for latter use"""
+        if not isinstance(value, int) or type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle a geometry with 4 sides """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
