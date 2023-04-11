4423d3024fd115a1ae5d0126f5b410816b85b603
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-11 22:51:37 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module for Student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dc = vars(self)
+        if not attrs:
+            return dc
+        newdc = dict()
+        for a in attrs:
+            if a in dc:
+                newdc[a] = dc[a]
+        return newdc
