2c45468a38448598d809469f15f2d2e2df9cb16b
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-11 00:13:04 +0000

    adding a new attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Write a function that adds a new attribute to an object if it’s possible:
-Raise a TypeError exception,
-with the message can't add new attribute if the object can’t have new attribute
-You are not allowed to use try/except
-You are not allowed to import any module
+"""Write a function that adds a new attribute to an object if it’s possibl
 """
 
 
