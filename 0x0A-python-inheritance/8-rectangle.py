5d8e0b38b7c045ea13e5ead2642e2b30da94634f
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-12 03:24:48 +0000

    big fix - importing problem

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """ a module with BaseGeometry calss"""
-
-
-class BaseGeometry:
-    """ A class with unimplimentd area"""
-    def area(self):
-        """ An area with no implimentstion"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates integers for latter use"""
-        if not isinstance(value, int) or type(value) == bool:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
