5958fd7158511cc5a8f18b80f1c3a2b6b65e3a0d
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-12 01:41:37 +0000

    stats printer

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""import sys
+for line in sys.stdin:
+    chrCounter += len(line)
+    print(counter)
+"""
+import sys
+import re
+result = []
+file_size = []
+counter = 0
+dc = {"200" : 0, "301" : 0, "400" : 0, "401" : 0, "403" : 0, "404" : 0, "405" : 0, "500" : 0}
+try:
+    for line in sys.stdin:
+        match = re.match(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)',line)
+        result.append(match.group(3))
+        dc[match.group(3)] += 1
+        file_size.append(int(match.group(4)))
+        counter +=1
+        if counter == 12:
+            raise KeyboardInterrupt()
+except KeyboardInterrupt:
+    pass
+finally:
+    for r in range(len(result)):
+        if r % 10 == 0:
+            print(f"File size: {sum(file_size[:r+10])}")
+            for d in dc:
+                if dc[d] != 0:
+                    print(f"{d}: {dc[d]}")
