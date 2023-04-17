fd45a58c4365cc97e9d63b40324269d5145c2ec8
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-17 21:40:11 +0000

    pystylecode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ constructor for Square class """
-        super().__init__(size, size, x, y, None)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
