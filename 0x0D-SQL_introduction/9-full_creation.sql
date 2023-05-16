8395e0ec039b2adc09049b762a58f2df171dc909
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-16 14:44:38 -0700

    create second table, sorted by scores, top scorers

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates a table called second_table and fills it with data
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table values (1, 'John', 10);
+INSERT INTO second_table values (2, 'Alex', 3);
+INSERT INTO second_table values (3, 'Bob', 14);
+INSERT INTO second_table values (4, 'George', 8);
