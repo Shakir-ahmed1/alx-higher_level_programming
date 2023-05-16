c903d9e16ffab8584a7b1b78ff4126283dd06af3
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-16 14:57:41 -0700

    Bug fix: checking if the table exist already

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table called second_table and fills it with data
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table values (1, 'John', 10);
 INSERT INTO second_table values (2, 'Alex', 3);
 INSERT INTO second_table values (3, 'Bob', 14);
