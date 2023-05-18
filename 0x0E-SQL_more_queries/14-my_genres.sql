a74623e3d342a062fc68e5b062c133dc1aa9e5e9
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-17 23:27:35 -0700

    fix: adding ordering by genre name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,5 @@
 SELECT tg.name
 FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
 LEFT JOIN tv_genres tg ON tg.id = tsg.genre_id
-WHERE ts.title = 'Dexter';
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name;
