b6674ab8d52f78ccdd0b7b7dcaf6968072b93266
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-17 23:24:44 -0700

    fix: removing aliases

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres of the show Dexter.
-SELECT tg.name AS name
+SELECT tg.name
 FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
 LEFT JOIN tv_genres tg ON tg.id = tsg.genre_id
 WHERE ts.title = 'Dexter';
