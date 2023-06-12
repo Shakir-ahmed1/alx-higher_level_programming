5581322ca6911fc9c57c4b77efe9a983048863a6
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-12 23:09:22 +0000

    bug fix: returning 1 for NaN files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function factorial (num) {
-  if (num === 0) return 1;
+  if (isNaN(num) || num === 0) return 1;
   else return num * factorial(num - 1);
 }
 const num = Number(process.argv[2]);
