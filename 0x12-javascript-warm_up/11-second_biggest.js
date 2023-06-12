b361a9e4bb2f86ebc5974032e89c3ff62d5e42c8
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-12 23:02:12 +0000

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const len = process.argv.length;
+let first, second;
+if (len <= 3) {
+  console.log(0);
+} else {
+  if (process.argv[2] > process.argv[3]) {
+    first = process.argv[2];
+    second = process.argv[3];
+  } else {
+    first = process.argv[3];
+    second = process.argv[2];
+  }
+  for (let i = 4; i < len; i++) {
+    if (process.argv[i] > first) {
+      second = first;
+      first = process.argv[i];
+    }
+  }
+  console.log(second);
+}
