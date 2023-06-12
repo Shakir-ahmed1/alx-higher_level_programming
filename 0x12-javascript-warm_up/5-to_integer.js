a8a70e83f5a47309288b97731d76510dbd9aa87f
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-12 22:09:54 +0000

    not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const num = Number(process.argv[2])
+const num = Number(process.argv[2]);
 if (isNaN(num)) console.log('Not a number');
-else console.log(`My number: ${num}`)
+else console.log(`My number: ${num}`);
