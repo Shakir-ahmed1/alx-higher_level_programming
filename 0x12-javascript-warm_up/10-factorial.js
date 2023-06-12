e41d7303f4c54d7cb84629ca3961b191465432e1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-12 22:46:06 +0000

    factorail

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (num === 0) return 1;
+  else return num * factorial(num - 1);
+}
+const num = Number(process.argv[2]);
+console.log(factorial(num));
