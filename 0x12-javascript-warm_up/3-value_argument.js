2f991666a948fc82523b98e71d3dea520784e707
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-13 00:24:55 +0000

    value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,5 +3,5 @@ let len = 0;
 process.argv.forEach((val, index) => {
   len++;
 });
-if (len === 2) console.log('No Arguments');
+if (len === 2) console.log('No Argument');
 else console.log(`${process.argv[2]}`);
