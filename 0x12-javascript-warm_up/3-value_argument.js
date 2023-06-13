183d35a083a5b6e000267fa600418eb843478699
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-13 00:27:02 +0000

    bugfix: output correction

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,5 +3,5 @@ let len = 0;
 process.argv.forEach((val, index) => {
   len++;
 });
-if (len === 2) console.log('No Argument');
+if (len === 2) console.log('No argument');
 else console.log(`${process.argv[2]}`);
