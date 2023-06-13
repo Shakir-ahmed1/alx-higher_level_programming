29652538ad120153923133da9aef3ad4de94ab7e
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-13 22:04:21 +0000

    new square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (!w || !h || w <= 0 || h <= 0) return {};
+    else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let temp = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        temp += 'X';
+      }
+      console.log(temp);
+      temp = '';
+    }
+  }
+
+  rotate () {
+    this.width = [this.height, this.height = this.width][0];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
