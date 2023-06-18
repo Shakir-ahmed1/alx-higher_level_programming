336bf94da41eee24786f1891a0f4e116c506e6b1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 19:55:13 +0000

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+cur = db.cursor()
+cur.execute("""
+SELECT c.name
+FROM states s JOIN cities c ON c.state_id = s.id
+WHERE s.name = '{}'
+        """.format(argv[4]))
+result = []
+for d in cur.fetchall():
+    result.append(d[0])
+print(", ".join(result))
