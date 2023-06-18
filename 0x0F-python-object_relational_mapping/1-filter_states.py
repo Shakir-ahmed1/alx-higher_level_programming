0e5c7a6159f746e3ccdf9513b832074c9197dbc8
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 17:31:57 +0000

    update: filling all requirments default arguments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,8 @@
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     db.query("SELECT * FROM states WHERE name LIKE 'N%'")
     r = db.store_result()
     for st in r.fetch_row(maxrows=0):
