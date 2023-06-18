9312486e36bb26a995d0e22290bcec4a9c967325
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 16:51:16 +0000

    filter with regexp operator

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetches all states in the state table which start with N"""
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db.query("SELECT * FROM states WHERE name REGEXP '^N'")
+    r = db.store_result()
+    for st in r.fetch_row(maxrows=0):
+        print(st)
