b61a342a21b634a0a7203fa7da28292288883ef8
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 23:59:27 +0000

    adding documentation and pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,9 +3,10 @@
 from model_state import Base, State
 from sqlalchemy import Column, String, Integer, ForeignKey
 
+
 class City(Base):
+    """ defines the cities table """
     __tablename__ = "cities"
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"))
-
