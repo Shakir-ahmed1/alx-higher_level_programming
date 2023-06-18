3d215dcfcd074e0be7f695455cf2c69acc032dab
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 21:35:30 +0000

    documentation and pystylecode

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+""" a base module that creates a python state object from db"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 Base = declarative_base()
 
+
 class State(Base):
+    """ defines the state table """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
