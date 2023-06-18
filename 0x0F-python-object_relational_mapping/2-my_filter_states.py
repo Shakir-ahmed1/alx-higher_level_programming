c80c567bdcde244b518f33a21c266ec3bd48396b
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 17:15:16 +0000

    get state form user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetches all states in the state table which start with N"""
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db.query("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    r = db.use_result()
+    for st in r.fetch_row(maxrows=0):
+        print(st)
