b1f3f68b06f509731912723ae4dc87240512c6bc
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 16:27:54 +0000

    initial commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ fetches all states in the state table"""
-import MySQLdb
-from sys import argv
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-db.query('SELECT * FROM states')
-r = db.store_result()
-for st in r.fetch_row(maxrows=0):
-    print(st)
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db.query('SELECT * FROM states')
+    r = db.store_result()
+    for st in r.fetch_row(maxrows=0):
+        print(st)
