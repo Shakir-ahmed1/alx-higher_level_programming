8fdb124fca8e498c8b16dca4d58fa71a4f285081
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 17:26:22 +0000

    update: store instead of use

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     from sys import argv
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     db.query("SELECT * FROM states WHERE name REGEXP '^N.*$' ORDER BY states.id")
-    r = db.use_result()
+    r = db.store_result()
     for st in r.fetch_row(maxrows=0):
         print(st)
