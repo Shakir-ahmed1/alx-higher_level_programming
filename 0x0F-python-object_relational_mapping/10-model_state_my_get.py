a822c98c6f8e53effaf16819f047e4318152b4cb
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 22:32:33 +0000

    get my id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" returns the id of the given state if it exists"""
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine(
+            f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name == argv[4]).one_or_none()
+    if result is not None:
+        print(result.id)
+    else:
+        print("Not found")
