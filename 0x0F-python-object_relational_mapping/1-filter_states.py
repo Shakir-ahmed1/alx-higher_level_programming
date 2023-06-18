f877ea9df9366b36a24da2a17a5a0de8d2bab783
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-18 17:36:00 +0000

    update: prefixing attribute names with table name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     from sys import argv
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-    db.query("SELECT * FROM states WHERE name LIKE 'N%'")
+    db.query("SELECT * FROM states WHERE states.name LIKE 'N%'")
     r = db.store_result()
     for st in r.fetch_row(maxrows=0):
         print(st)
