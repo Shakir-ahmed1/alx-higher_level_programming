972b4d68f6777ad8dbfe6a73db1d02baf999d5f1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-19 00:08:36 +0000

    replacing query by execute

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE BINARY states.name LIKE 'N%'")
     for r in cur.fetchall():
         print(r)
     db.close()
