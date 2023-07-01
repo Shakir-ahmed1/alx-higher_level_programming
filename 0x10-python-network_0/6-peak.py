2e94ce66d4b1657f3d3c8c2a67fe88a006afe011
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-30 19:01:15 -0700

    peaks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module that finds peak """
+
+def find_peak(list_of_integers):
+    """ finds a pick """
+    length = len(list_of_integers)
+    li = list_of_integers
+    for a in range(length):
+        if a == 0 and length > 1 and li[0] >= li[1]:
+            return li[0]
+        elif a > 1 and a < length - 1:
+            if li[a - 1] < li[a] and li[a + 1] <= li[a]:
+                return li[a]
+        elif a == length - 1 and a > 1 and li[-1] >= li[-2]:
+            return li[0]
+
