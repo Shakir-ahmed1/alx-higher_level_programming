76d657dcd540114d468b18d8d99e3375872f2e20
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-07-02 13:48:26 -0700

    bugfix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ sends a POST request and provides an email """
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 from sys import argv
 email = {'email': argv[2]}
-data = urllib.parse.urlencode(email)
+data = parse.urlencode(email)
 data = data.encode('ascii')
-req = urllib.request.Request(argv[1], data)
-with urllib.request.urlopen(req) as response:
+req = request.Request(argv[1], data)
+with request.urlopen(req) as response:
     print(response.read().decode('utf-8'))
