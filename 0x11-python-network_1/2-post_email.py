47c7feab76f45f8858a726dcfe7d084f98fdcff1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-07-02 20:34:25 +0000

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/uar/bin/python3
+""" sends a POST request and provides an email """
+import urllib.request
+import urllib.parse
+from sys import argv
+email = {'email': argv[2]}
+data = urllib.parse.urlencode(email)
+data = data.encode('ascii')
+req = urllib.request.Request(argv[1], data)
+with urllib.request.urlopen(req) as response:
+    print(response.body())
