5a54c6e59503eab7fd5f752812ca772ac70fd6a9
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-07-02 16:46:53 -0700

    bugfix: more inclusive error handling

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
             print(f"[{js['id']}] {js['name']}")
         else:
             print("No result")
-    except requests.exceptions.JSONDecodeError:
+    except:
         print("Not a valid JSON")
