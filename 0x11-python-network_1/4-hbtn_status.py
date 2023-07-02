4b5bffceb7540795ff8ad24ac4a2bdfc872a3ed3
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-07-02 14:07:52 -0700

    what is my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" what is my status """
+if __name__ == "__main__":
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
