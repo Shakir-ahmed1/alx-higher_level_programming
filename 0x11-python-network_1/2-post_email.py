0a6e111e1da2cc9efe41539600130938c84d3ca3
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-07-02 13:53:16 -0700

    requirment: must not run on import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ sends a POST request and provides an email """
-from urllib import request, parse
-from sys import argv
-email = {'email': argv[2]}
-data = parse.urlencode(email)
-data = data.encode('ascii')
-req = request.Request(argv[1], data)
-with request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    email = {'email': argv[2]}
+    data = parse.urlencode(email)
+    data = data.encode('ascii')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
