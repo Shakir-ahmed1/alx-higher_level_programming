950ceebd08113dfb24e80ba3cc690e3bdacce406
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-04 17:30:21 +0000

    pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,8 @@ def find_peak(list_of_integers):
     mid = len(list_of_integers) // 2
     left = list_of_integers[:mid]
     right = list_of_integers[mid+1:]
-    if (not left or list_of_integers[mid] >= left[-1]) and (not right or list_of_integers[mid] >= right[0]):
+    if (not left or list_of_integers[mid] >= left[-1]) and (
+            not right or list_of_integers[mid] >= right[0]):
         return list_of_integers[mid]
     elif left and list_of_integers[mid] < left[-1]:
         return find_peak(left)
