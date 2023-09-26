625cef8244bdbe6e78f74232701c15a4c5f02a45
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-26 20:59:03 +0000

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (err, response, body) => {
+  if (err) console.error(err);
+  else console.log('code:', response.statusCode);
+});
