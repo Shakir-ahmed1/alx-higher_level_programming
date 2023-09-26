61598886dcd7db8639c3d48edc39a193a71b4a6f
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-26 23:33:37 +0000

    displey employee to do status

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.err(err);
+  } else {
+    const todos = JSON.parse(body);
+    const result = {};
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].userId in result) {
+        if (todos[i].completed === true) result[todos[i].userId] += 1;
+      } else {
+        result[todos[i].userId] = 0;
+      }
+    }
+    console.log(result);
+  }
+}
+);
