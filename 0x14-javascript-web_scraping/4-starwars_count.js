9234e9f98668a3ca695284490e79d07a8d170cd5
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-26 23:02:47 +0000

    searchs for 18 in characters

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,9 @@ request(process.argv[2], function (err, response, body) {
     const response = JSON.parse(body);
     let count = 0;
     for (let index = 0; index < response.results.length; index++) {
-      if (response.results[index].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) count++;
+      response.results[index].characters.forEach(element => {
+	if (element.includes('/18/')) count++;
+      });
     }
     console.log(count);
   }
