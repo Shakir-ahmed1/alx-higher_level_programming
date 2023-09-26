d6ef9c209fa496a09e28967c488f4212f8ddc874
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-26 23:39:18 +0000

    displey employee to do status

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,10 +7,12 @@ request(process.argv[2], (err, response, body) => {
     const todos = JSON.parse(body);
     const result = {};
     for (let i = 0; i < todos.length; i++) {
-      if (todos[i].userId in result) {
-        if (todos[i].completed === true) result[todos[i].userId] += 1;
-      } else {
-        result[todos[i].userId] = 1;
+      if (todos[i].completed === true) {
+        if (todos[i].userId in result) {
+          result[todos[i].userId] += 1;
+        } else {
+          result[todos[i].userId] = 1;
+        }
       }
     }
     console.log(result);
