cb14f7206cc3a9970347666aacec9df62f4d3d36
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-26 20:34:11 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,7 @@
 // const request = require('request');
 const fs = require('fs');
 
-text = fs.readFile(process.argv[2], 'utf-8', function(err, data) {
-   if (err) console.error(err);
-   else console.log(data)});
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) console.error(err);
+  else console.log(data);
+});
