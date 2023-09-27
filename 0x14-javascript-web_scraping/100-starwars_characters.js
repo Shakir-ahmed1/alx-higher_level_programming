e39c3492772a802220cfe22aff1394be8fbdac35
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-27 00:06:51 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,21 +2,20 @@
 const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, response, body) => {
   if (err) {
-	  console.error(err);
+    console.error(err);
   } else {
-	  console.log
-	  const data = JSON.parse(body);
-          for (i = 0; i < data.characters.length; i++) {
-	    request(data.characters[i], (e, r, b) => {
-		if (e) {
-			console.error(e);
-		} else {
-		const person = JSON.parse(b);
-		console.log(person.name);
-		}
-	    }
-	    );
-	  }
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.characters.length; i++) {
+      request(data.characters[i], (e, r, b) => {
+        if (e) {
+          console.error(e);
+        } else {
+          const person = JSON.parse(b);
+          console.log(person.name);
+        }
+      }
+      );
+    }
   }
 }
 );
