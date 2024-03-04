d5f9386c8d9b63ecc2ebd2c2a36c6416d35188d0
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-04 13:12:06 +0300

    advanced

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include "Python.h"
 /**
  * print_python_string - prints python string using c
@@ -5,5 +7,24 @@
  */
 void print_python_string(PyObject *p)
 {
-	(void) p;
+	PyObject *str, *str2;
+
+	(void)str2;
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	str2 = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(str));
 }
