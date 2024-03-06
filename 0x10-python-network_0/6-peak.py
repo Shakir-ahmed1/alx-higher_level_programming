26495a093580674d35d28a719a2777ea0cad96d6
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-06 21:44:09 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,16 +4,19 @@
 
 def find_peak(list_of_integers):
     """ finds a pick using the same principles as binary search"""
-    if not list_of_integers or len(list_of_integers) == 1:
-        return list_of_integers[0] if list_of_integers else None
 
-    mid = len(list_of_integers) // 2
-    left = list_of_integers[:mid]
-    right = list_of_integers[mid+1:]
-    if (not left or list_of_integers[mid] >= left[-1]) and (
-            not right or list_of_integers[mid] >= right[0]):
-        return list_of_integers[mid]
-    elif left and list_of_integers[mid] < left[-1]:
-        return find_peak(left)
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(right)
+        return find_peak(list_of_integers[mid + 1:])
