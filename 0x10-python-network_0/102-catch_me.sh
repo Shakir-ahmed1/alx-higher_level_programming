b90174a061068a4f973434f34e247e1e78890985
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-06 21:33:59 +0300

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Make a request to the server
 curl -s -X PUT 0.0.0.0:5000/catch_me -L -d "user_id=98" -H "Origin: HolbertonSchool" -H "Referer: HolbertonSchool" -H "Content-Type: application/json" -d "{\"user_id\": 98}" -H "Expect: " -H "Transfer-Encoding: " -H "Connection: close"
 
